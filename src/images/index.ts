74d5794789c297616b0f5151255e927cc143f782
rajabzoda2810 <122232005+rajabzoda2810@users.noreply.github.com>
2026-07-17 19:24:59 +0500

    Rename main character image to RX_Empire.png

--- src/images/index.ts
+++ src/images/index.ts
@@ -4,7 +4,7 @@ import dollarCoin from "./dollar-coin.png";
 import dailyReward from "./daily-reward.png";
 import dailyCipher from "./daily-cipher.png";
 import dailyCombo from "./daily-combo.png";
-import mainCharacter from "./main-character.png";
+import mainCharacter from "./RX_Empire.png";
 import hamsterCoin from "./hamster-coin.png";
 
 export {
